a38416558107350090e901fc24eee3204b282042
RLonskiy <rlonskiy@luxoft.com>
2019-09-25 13:22:40 +0300

    Updates

---
+++ precache-manifest.7afb72f209f00b1f56cf2533123402ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7ce32b4a3d3817064deb68ffaa6a13",
+    "url": "/Benchmark/index.html"
+  },
+  {
+    "revision": "cac79c52994ed954fec3",
+    "url": "/Benchmark/static/css/main.294e00b4.chunk.css"
+  },
+  {
+    "revision": "c6792905ec7b033ca645",
+    "url": "/Benchmark/static/js/2.f153b31b.chunk.js"
+  },
+  {
+    "revision": "cac79c52994ed954fec3",
+    "url": "/Benchmark/static/js/main.680b292a.chunk.js"
+  },
+  {
+    "revision": "192d49e65115a11a4c29",
+    "url": "/Benchmark/static/js/runtime-main.beb684b8.js"
+  }
+]);
